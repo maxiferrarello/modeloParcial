b85ca577d4b5490b2bd44490b3bba97cff15c870
maxif <maxiferrarello@gmail.com>
2026-05-20 11:43:21 -0300

    cambios en marcha

--- modeloParcial/src/main/java/com/miModeloParcial/modeloParcial/Estantes/IServicioEstantes.java
+++ modeloParcial/src/main/java/com/miModeloParcial/modeloParcial/Estantes/IServicioEstantes.java
@@ -2,9 +2,12 @@ package com.miModeloParcial.modeloParcial.Estantes;
 
 import com.miModeloParcial.modeloParcial.Estantes.Dominio.DTO.EstanteDTO;
 
-public interface IServicioEstantes {
+import java.util.List;
 
+public interface IServicioEstantes {
     /// crud obligatorios
     public EstanteDTO guardar (EstanteDTO estanteDTO);
-
+    public void borrar(Long id);
+    public EstanteDTO actualizar (Long id,EstanteDTO estanteDTO);
+    public List<EstanteDTO> buscarTodos ();
 }
--- modeloParcial/src/main/java/com/miModeloParcial/modeloParcial/Estantes/ServicioEstantes.java
+++ modeloParcial/src/main/java/com/miModeloParcial/modeloParcial/Estantes/ServicioEstantes.java
@@ -1,4 +1,41 @@
 package com.miModeloParcial.modeloParcial.Estantes;
 
-public class ServicioEstantes {
+import com.miModeloParcial.modeloParcial.Estantes.Dominio.DTO.EstanteDTO;
+import com.miModeloParcial.modeloParcial.Estantes.Dominio.Mappers.EstanteMapper;
+import com.miModeloParcial.modeloParcial.Estantes.Repositorio.RepositorioEstantes;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ServicioEstantes implements IServicioEstantes{
+
+    private final RepositorioEstantes repositorioEstantes;
+    private final EstanteMapper estanteMapper;
+
+
+    @Override
+    public EstanteDTO guardar(EstanteDTO estanteDTO) {
+
+        EntidadEstantes guardado = repositorioEstantes.save(estanteMapper.toEntity(estanteDTO));
+
+        return estanteMapper.toDto(guardado);
+    }
+
+    @Override
+    public void borrar(Long id) {
+
+    }
+
+    @Override
+    public EstanteDTO actualizar(Long id, EstanteDTO estanteDTO) {
+        return null;
+    }
+
+    @Override
+    public List<EstanteDTO> buscarTodos() {
+        return List.of();
+    }
 }
